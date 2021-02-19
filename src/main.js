57d1bc64efe4b0fc8e357a7e3bbeaefd4d1db835
568074019@qq.com <568074019@qq.com>
2021-02-19 21:51:34 +0800

    初始代码

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+
+import 'normalize.css/normalize.css'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss'
+import App from './App'
+import store from './store'
+import router from './router'
+
+import '@/icons'
+import '@/permission'
+
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
